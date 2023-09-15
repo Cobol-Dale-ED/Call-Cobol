0337324f61d46370a3227bd93080d15666f4e0b3
Laptop Mac <gs@gregoryshields.net>
2023-09-14 17:17:33 -0700

    Uppercase keywords and add .gitignore file.

--- main_app.cbl
+++ main_app.cbl
@@ -3,70 +3,71 @@
       * Date: 2020-04-16
       * Updated: 2022-04-13
       * Purpose: Main application calls sub-app by content and reference
-      * Tectonics: cobc -x main_app.cbl sub.cbl -o a.out
+      * Tectonics: cobc -x main_app.cbl sub.cbl -o main_app.exe
       ******************************************************************
-       identification division.
-       program-id. main-app.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. main-app.
 
-       data division.
+       DATA DIVISION.
 
-       file section.
+       FILE SECTION.
 
-       working-storage section.
+       WORKING-STORAGE SECTION.
 
        01 ws-group-1.
-           05 ws-item-1                        pic x(10).
-           05 ws-item-2                        pic x(10).
+           05 ws-item-1                        PIC x(10).
+           05 ws-item-2                        PIC x(10).
 
-       procedure division.
-       main-procedure.
-           display space
-           display "Enter value for #1: " with no advancing
-           accept ws-item-1
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY SPACE
+           DISPLAY "Enter value for #1: " WITH NO ADVANCING
+           ACCEPT ws-item-1
 
-           display "Enter value for #2: " with no advancing
-           accept ws-item-2.
+           DISPLAY "Enter value for #2: " WITH NO ADVANCING
+           ACCEPT ws-item-2.
 
-           perform display-message
+           PERFORM display-message
 
       *> Calling by content, the variables passed will not be modified
       *> upon return to the main application.
-           display "Calling sub program by content:"
-           call "sub-app" using
-               by content ws-item-1
-               by content ws-item-2
-           end-call
-           perform display-message
+           DISPLAY "Calling sub program by content:"
+           CALL "sub-app" USING
+               BY CONTENT ws-item-1
+               BY CONTENT ws-item-2
+           END-CALL
+           PERFORM display-message
 
       *> Calling by reference (default) the variables can be modified by
       *> the called sub program. Note that the working-storage variables
       *> of the sub program retain their values between calls where the
       *> linkage section variables do not.
-           display "Second call of sub program should retain WS values."
-           display "Calling sub program by reference:"
-           call "sub-app" using
+           DISPLAY "Second call of sub program should retain WS values."
+           DISPLAY "Calling sub program by reference:"
+           CALL "sub-app" USING
                ws-item-1 ws-item-2
-           end-call
-           perform display-message
+           END-CALL
+           PERFORM display-message
 
       *> Cancelling the sub program will reset all variables in the
       *> working storage section back to their original values.
-           display "Cancelling sub program"
-           cancel "sub-app"
-           display "Calling sub program. WS values should be reset:"
-           call "sub-app" using
+           DISPLAY "Cancelling sub program"
+           CANCEL "sub-app"
+           DISPLAY "Calling sub program. WS values should be reset:"
+           CALL "sub-app" USING
                ws-item-1 ws-item-2
-           end-call
-           perform display-message
+           END-CALL
+           PERFORM display-message
 
 
-           stop run.
+           STOP run.
 
-       display-message.
-           display space
-           display "-----------------------------------------------"
-           display "Main app: " ws-group-1
-           exit paragraph.
+       DISPLAY-MESSAGE.
+           DISPLAY space
+           DISPLAY "-----------------------------------------------"
+           DISPLAY "Main app: " ws-group-1
+           EXIT paragraph.
 
 
-       end program main-app.
+       END PROGRAM main-app.
+
